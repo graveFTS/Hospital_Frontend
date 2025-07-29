8a132793b4265dc9beb62cb5e5e41bd781bbc7d9
Dipsekhar Maity <dipsekharmaity9723@gmail.com>
2025-07-29 17:36:31 +0530

    commit pngs

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -6,9 +6,9 @@ const AboutUs = () => {
     <>
       <Hero
         title={"Learn More About Us | ZeeCare Medical Institute"}
-        imageUrl={"/about.png"}
+        imageUrl={"about.png"}
       />
-      <Biography imageUrl={"/whoweare.png"} />
+      <Biography imageUrl={"whoweare.png"} />
     </>
   );
 };
